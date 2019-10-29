b8667c1a53a505a718e1c6291354de664e62d078
iizumi100 <q104003s@mail.kyutech.jp>
2019-10-29 13:20:04 +0900

    TeachingBoothVR

---
+++ Assets/CubeTukamuman2.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeTukamuman2 : MonoBehaviour
+{
+
+    public static Vector3 TerceroLocalPos;
+    public static Quaternion TerceroLocalRot;
+    //Rigidbody rbo;
+    //Collider m_object, finger;
+    int c = 0;
+    private Vector3 phantom;
+    public static int grabpreparation;
+    public int aa;
+    float rotY,rotY2;
+    //public static Transform invisibletr;
+    
+
+    void Start()
+    {
+
+        GameObject Finger = GameObject.Find("Finger_F1");
+
+    }
+
+ 
+    void Update()
+    {
+        if (OVRInput.Get(OVRInput.RawButton.X) == false)
+        {
+     
+            this.gameObject.transform.parent = null;
+            grabpreparation = 0;
+            c = 0;
+            aa = 0;
+
+            //InvisibleObjectとの親子関係を解除
+            int ObjCount = this.transform.childCount;
+            if (ObjCount == 1)
+            {
+                GameObject ChildGameObject1 = transform.GetChild(0).gameObject;
+                ChildGameObject1.transform.parent = null;
+            }
+            print("X解除");
+
+        }
+
+        if (OVRInput.Get(OVRInput.RawButton.X))
+        {
+
+            if(c == 0)
+            {
+                // 回転の中心を衝突点の中心に持っていく
+                Vector3 hitpos3 = (CollisionPoint.hitPos + CollisionPoint2.hitPos2) / 2;
+                this.gameObject.transform.position = hitpos3;
+                //if (CubeTukamuman3.start == 1)
+                //{
+                    this.gameObject.transform.parent = GameObject.Find("LeftHandAnchor").transform;
+               // }
+                c = 1;
+            }
+            
+            
+            //float posY = this.gameObject.transform.position.y;
+            //this.gameObject.transform.position = new Vector3(hitpos3.x, hitpos3.y, hitpos3.z);
+
+            grabpreparation = 1;
+
+            
+
+            if (CubeTukamuman3.grabpreparation2 == 2)
+            {
+                if (aa == 0)
+                {
+                    float rotY = this.gameObject.transform.rotation.eulerAngles.y;
+                    
+                    aa = 1;
+                }
+                float rotY2 = LeftHandPose.lefthandq.eulerAngles.y;
+                //float rotS = GameObject.Find("LeftHandAnchor").transform.rotation.eulerAngles.y;
+                //float rotY2 = LeftHandPose.lefthandq.eulerAngles.y;
+                this.gameObject.transform.localRotation = Quaternion.Euler(-90, rotY - rotY2, 0);
+                //this.gameObject.transform.localRotation = Quaternion.Euler(0, 0, 0);
+                grabpreparation = 3;
+                print("ワシじゃよ2改23");
+                c = 4;
+                
+
+            }
+
+           
+        }
+
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        
+    }
+
+
+}
---
+++ Assets/CubeTukamuman3.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeTukamuman3 : MonoBehaviour
+{
+
+    public static Vector3 TerceroLocalPos;
+    public static Quaternion TerceroLocalRot;
+    int freeze,d;
+    Rigidbody rbo;
+    Collider m_object, finger;
+    int c;
+    public static int start;
+    private Vector3 phantom;
+    public static int grabpreparation2;
+    public static bool grabswitch = false;
+    //public static Rigidbody rb2;
+    //public string save_name;
+    //public bool save_name_tf = false;
+
+
+    void Start()
+    {
+        rbo = this.transform.GetComponent<Rigidbody>();
+        m_object = GetComponent<Collider>();
+        GameObject Finger = GameObject.Find("Finger_F1");
+    }
+
+    void Update()
+    {
+
+        //print(this.gameObject.transform.parent.name);
+
+        if (OVRInput.Get(OVRInput.RawButton.X) == false)
+        {
+            
+            //string parent;
+            //parent = this.gameObject.transform.parent.tag;
+            //if (parent == "robothand")
+            //{
+                //this.gameObject.transform.parent = null;
+            //}
+            var rb = GetComponent<Rigidbody>();
+            rb.isKinematic = false;
+            print("離したはず");
+            m_object.isTrigger = false;
+            grabpreparation2 = 0;
+            start = 0;
+            
+            /*if (save_name_tf == true)
+            {
+                this.gameObject.name = save_name;
+                save_name_tf = false;
+                save_name = null;
+            }*/
+            
+        }
+
+        if (OVRInput.Get(OVRInput.RawButton.X))
+        {
+            finger = this.gameObject.GetComponent<Collider>();
+            finger.isTrigger = false;
+        }
+    }
+
+    void OnCollisionStay(Collision col)
+    {
+
+        if (col.gameObject.tag == "Finger" && OVRInput.Get(OVRInput.RawButton.X) && this.gameObject.name != "HV_assembled")
+        {
+
+            var rb2 = GetComponent<Rigidbody>();
+            rb2.constraints = RigidbodyConstraints.FreezePosition;
+            start = 1;
+            if (CubeTukamuman2.grabpreparation == 1)
+            {
+                //GameObject.Find("InvisibleObject").transform.rotation = this.gameObject.transform.rotation;
+                this.gameObject.transform.parent = GameObject.Find("InvisibleObject").transform;
+                grabpreparation2 = 2;
+                print("ワシじゃよ1");
+                
+                //save_name = this.gameObject.name;
+                // this.gameObject.name = "HV";
+                //save_name_tf = true;
+                //var rb2 = GetComponent<Rigidbody>();
+                //rb2.constraints = RigidbodyConstraints.FreezePosition;
+
+            }
+
+            if(CubeTukamuman2.grabpreparation == 3)
+            {
+                //Rigidbodyを取得
+                
+
+                //isKinematicをオンにする
+                rb2.isKinematic = true;
+
+                GameObject.Find("TerceroPhantom").transform.parent = this.gameObject.transform;
+                TerceroLocalPos = GameObject.Find("TerceroPhantom").transform.localPosition;
+                TerceroLocalRot = GameObject.Find("TerceroPhantom").transform.localRotation;
+                print(TerceroLocalPos);
+                print(TerceroLocalRot);
+                print("ワシじゃよ3");
+                grabswitch = true;
+
+                //var rb2 = GetComponent<Rigidbody>();
+                //rb2.constraints = RigidbodyConstraints.FreezePosition;
+                grabpreparation2 = 4;
+            }
+
+            
+        }
+
+    }
+
+}
